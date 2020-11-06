c4e418c33d364880cbb9f050954f3855059df8b8
SirishaSomayajula <saisirisha568@gmail.com>
2020-11-06 08:53:12 +0530

    testing

---
+++ tests1/test_book.py
@@ -0,0 +1,23 @@
+from odoo.tests.common import TransactionCase
+class TestBook(TransactionCase):
+    def setUp(self, *args, **kwargs):
+       result = super().setUp(*args, **kwargs)
+       user_admin = self.env.ref('base.user_admin')
+       self.env= self.env(user=user_admin)
+       self.Book = self.env['library.book']
+       self.book_ode = self.Book.create({
+          'name': 'Revolution 2020',
+          'isbn': '978-81-2911-880-6'})
+       return result
+    def test_create(self):
+       "Test Books are active"
+        self.assertEqual(self.book_ode.active, True)
+    def test_check_isbn(self):
+       "Check valid ISBN"
+       self.assertTrue(self.book_ode._check_isbn)
+        
+        
+        
+        
+        
+
